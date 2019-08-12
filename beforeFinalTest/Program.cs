67823769cb5f2ce13a1775944a26ac7647c9021c
Thi Thu Ha Nguyen <tnguy655@my.centennialcollege.ca>
2019-08-12 15:13:29 -0400

    added form

--- beforeFinalTest/Program.cs
+++ beforeFinalTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using beforeFinalTest.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace beforeFinalTest
 {
     static class Program
     {
+        
+        public static CharacterGenerationForm characterForm;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,8 @@ namespace beforeFinalTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-          
+             characterForm= new CharacterGenerationForm();
+            Application.Run(characterForm);
         }
     }
 }
---
+++ beforeFinalTest/Views/CharacterGenerationForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace beforeFinalTest.Views
+{
+    public partial class CharacterGenerationForm : beforeFinalTest.Views.MasterForm
+    {
+        public CharacterGenerationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
